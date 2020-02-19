9df5e757c00152759ffe83d589cdf8255177577c
王祥飞 <807102252@qq.com>
2020-02-19 21:57:22 +0800

    更新JDBC连接数据库模块

---
+++ accessing-data-jdbc/src/main/java/com/example/accessingdatajdbc/pojo/Customer.java
@@ -0,0 +1,52 @@
+package com.example.accessingdatajdbc.pojo;
+
+/**
+ * 功能描述：
+ * Customer实体类
+ * @author wangxiangfei
+ * @version 1.0
+ * @date 2020/2/19 21:47
+ **/
+public class Customer {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
